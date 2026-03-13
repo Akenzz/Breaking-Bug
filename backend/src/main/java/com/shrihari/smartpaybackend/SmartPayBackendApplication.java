2116766683ae9fd7149622fb76eaee394f19a0fe
shrihari_des18 <shriharideshpande901@gmail.com>
2026-03-13 12:40:14 +0530

    init commit backend

---
+++ backend/src/main/java/com/shrihari/smartpaybackend/SmartPayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shrihari.smartpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPayBackendApplication.class, args);
+    }
+
+}
